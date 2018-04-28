73848e853f028d5913e9c4a56d9a3f31a0160573
Sergey Klevakin <justerest@yandex.ru>
2018-04-28 20:26:12 +0500

    refactor: Home component into one file (because simple code part)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <HelloWorld msg="Vue.js + TypeScript App" />
   </div>
 </template>
 
-<script lang="ts" src="./Home.ts">
+<script>
+import HelloWorld from '@/components/HelloWorld';
+
+export default {
+  components: {
+    HelloWorld,
+  },
+};
 </script>
