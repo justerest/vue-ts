087dac68c350c2c4bd784c680eef53f4771b43f6
Sergey Klevakin <justerest@yandex.ru>
2018-04-28 20:27:51 +0500

    refactor(HelloWorldComponent): counter on new line

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-    <h1 @click="incr">{{ msg + count }}</h1>
+    <h1 @click="incr">{{msg}}</h1>
+    <h2>{{count}}</h2>
     <p>
       For guide and recipes on how to configure / customize this project,<br> check out the
       <a href="https://github.com/vuejs/vue-cli/tree/dev/docs" target="_blank">vue-cli documentation</a>.
